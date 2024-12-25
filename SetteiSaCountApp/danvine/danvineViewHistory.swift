8e0728655f357b19a91b7eb0bef69a7de52fe626
TY317 <musicman_stingray_f91@yahoo.co.jp>
2024-12-25 22:44:54 +0900

    ver1.8.0開発中

---
+++ SetteiSaCountApp/danvine/danvineViewHistory.swift
@@ -0,0 +1,330 @@
+//
+//  danvineViewHistory.swift
+//  SetteiSaCountApp
+//
+//  Created by 横田徹 on 2024/12/25.
+//
+
+import SwiftUI
+import TipKit
+
+struct danvineViewHistory: View {
+    @ObservedObject var danvine = Danvine()
+    @State var isShowAlert: Bool = false
+    @State var isShowDataInputView = false
+    @State private var orientation: UIDeviceOrientation = UIDevice.current.orientation
+    @State private var lastOrientation: UIDeviceOrientation = .portrait // 直前の向き
+    @FocusState var isFocused: Bool
+    let scrollViewHeightPortrait = 250.0
+    let scrollViewHeightLandscape = 150.0
+    @State var scrollViewHeight = 250.0
+    let spaceHeightPortrait = 250.0
+    let spaceHeightLandscape = 0.0
+    @State var spaceHeight = 250.0
+    
+    var body: some View {
+        List {
+            // //// 履歴表示
+            Section {
+                ScrollView {
+                    // 配列のデータが0以上なら履歴表示
+                    let gameArray = decodeIntArray(from: danvine.gameArrayData)
+                    if gameArray.count > 0 {
+                        ForEach(gameArray.indices, id: \.self) { index in
+                            let viewIndex = gameArray.count - index - 1
+                            HStack {
+                                // ゲーム数
+                                if gameArray.indices.contains(viewIndex) {
+                                    Text("\(gameArray[viewIndex])")
+                                        .lineLimit(1)
+                                        .frame(maxWidth: .infinity)
+                                } else {
+                                    Text("-")
+                                        .frame(maxWidth: .infinity)
+                                }
+                                // ボーナス種類
+                                let bonusArray = decodeStringArray(from: danvine.bonusArrayData)
+                                if bonusArray.indices.contains(viewIndex) {
+                                    Text("\(bonusArray[viewIndex])")
+                                        .lineLimit(1)
+                                        .frame(maxWidth: .infinity)
+                                        .font(.footnote)
+                                } else {
+                                    Text("-")
+                                        .frame(maxWidth: .infinity)
+                                }
+                                // 当選契機
+                                let triggerArray = decodeStringArray(from: danvine.triggerArrayData)
+                                if triggerArray.indices.contains(viewIndex) {
+                                    Text("\(triggerArray[viewIndex])")
+                                        .lineLimit(1)
+                                        .frame(maxWidth: .infinity)
+                                        .font(.footnote)
+                                } else {
+                                    Text("-")
+                                        .frame(maxWidth: .infinity)
+                                }
+                                // ST当否
+                                let stHitArray = decodeStringArray(from: danvine.stHitArrayData)
+                                if stHitArray.indices.contains(viewIndex) {
+                                    Text("\(stHitArray[viewIndex])")
+                                        .lineLimit(1)
+                                        .frame(maxWidth: .infinity)
+                                } else {
+                                    Text("-")
+                                        .frame(maxWidth: .infinity)
+                                }
+                            }
+                            Divider()
+                        }
+                    }
+                    // //// 配列のデータ数が0なら履歴なしを表示
+                    else {
+                        HStack {
+                            Spacer()
+                            Text("履歴なし")
+                                .font(.title)
+                            Spacer()
+                        }
+                        .padding(.top)
+                    }
+                }
+                .frame(height: self.scrollViewHeight)
+                
+                // //// 登録、1行削除ボタン
+                HStack {
+                    Spacer()
+                    Button {
+                        if danvine.minusCheck {
+                            let gameArray = decodeIntArray(from: danvine.gameArrayData)
+                            if gameArray.count > 0 {
+                                danvine.removeLastHistory()
+                                UINotificationFeedbackGenerator().notificationOccurred(.success)
+                            }
+                        } else {
+                            isShowDataInputView.toggle()
+                        }
+                    } label: {
+                        if danvine.minusCheck {
+                            Image(systemName: "minus")
+                        } else {
+                            Image(systemName: "plus")
+                        }
+                    }
+                    .buttonStyle(PlusDeleatButtonStyle(MinusBool: danvine.minusCheck))
+                    .sheet(isPresented: $isShowDataInputView) {
+                        danvineSubViewDataInput(danvine: danvine)
+                            .presentationDetents([.large])
+                    }
+                    Spacer()
+                }
+                
+            } header: {
+                unitHeaderHistoryColumns(
+                    column1Bool: false,
+                    column2: "実ゲーム数",
+                    column3: "ボナ種類",
+                    column4: "当選契機",
+                    column5: "ST当否"
+                )
+            }
+            
+            // //// 初当り
+            Section {
+                // 通常ゲーム数
+                unitResultCount2Line(
+                    title: "通常G数",
+                    count: $danvine.playGameSum
+                )
+                // //// ボーナス初当り
+                HStack {
+                    // ボーナス回数
+                    unitResultCount2Line(title: "ボーナス回数", count: $danvine.bonusCount)
+                    // ボーナス確率
+                    unitResultRatioDenomination2Line(
+                        title: "ボーナス確率",
+                        count: $danvine.bonusCount,
+                        bigNumber: $danvine.playGameSum,
+                        numberofDicimal: 0
+                    )
+                }
+                // //// ST初当り
+                HStack {
+                    // ST回数
+                    unitResultCount2Line(title: "ST回数", count: $danvine.stCount)
+                    // ST確率
+                    unitResultRatioDenomination2Line(
+                        title: "ST確率",
+                        count: $danvine.stCount,
+                        bigNumber: $danvine.playGameSum,
+                        numberofDicimal: 0
+                    )
+                }
+                // //// 参考情報リンク
+                unitLinkButton(
+                    title: "初当り確率",
+                    exview: AnyView(
+                        unitExView5body2image(
+                            title: "ボーナス、ST 初当り確率",
+                            image1: Image("danvineHitRatio")
+                        )
+                    )
+                )
+            } header: {
+                Text("初当り")
+            }
+        }
+        // //// 画面の向き情報の取得部分
+        .onAppear {
+            // ビューが表示されるときにデバイスの向きを取得
+            self.orientation = UIDevice.current.orientation
+            // 向きがフラットでなければlastOrientationの値を更新
+            if self.orientation.isFlat {
+                
+            }
+            else {
+                self.lastOrientation = self.orientation
+            }
+            if orientation.isLandscape || (orientation.isFlat && lastOrientation.isLandscape) {
+                self.scrollViewHeight = self.scrollViewHeightLandscape
+                self.spaceHeight = self.spaceHeightLandscape
+            } else {
+                self.scrollViewHeight = self.scrollViewHeightPortrait
+                self.spaceHeight = self.spaceHeightPortrait
+            }
+            // デバイスの向きの変更を監視する
+            NotificationCenter.default.addObserver(forName: UIDevice.orientationDidChangeNotification, object: nil, queue: .main) { _ in
+                self.orientation = UIDevice.current.orientation
+                // 向きがフラットでなければlastOrientationの値を更新
+                if self.orientation.isFlat {
+                    
+                }
+                else {
+                    self.lastOrientation = self.orientation
+                }
+                if orientation.isLandscape || (orientation.isFlat && lastOrientation.isLandscape) {
+                    self.scrollViewHeight = self.scrollViewHeightLandscape
+                    self.spaceHeight = self.spaceHeightLandscape
+                } else {
+                    self.scrollViewHeight = self.scrollViewHeightPortrait
+                    self.spaceHeight = self.spaceHeightPortrait
+                }
+            }
+        }
+        .onDisappear {
+            // ビューが非表示になるときに監視を解除
+            NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
+        }
+        .navigationTitle("初当たり履歴")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .automatic) {
+                HStack {
+                    // //// マイナスチェック
+                    unitButtonMinusCheck(minusCheck: $danvine.minusCheck)
+                    // /// リセット
+                    unitButtonReset(isShowAlert: $isShowAlert, action: danvine.resetHistory)
+                        .popoverTip(tipUnitButtonReset())
+                }
+            }
+        }
+    }
+}
+
+// /////////////////////
+// データインプット画面
+// /////////////////////
+struct danvineSubViewDataInput: View {
+    @ObservedObject var danvine: Danvine
+    @Environment(\.dismiss) private var dismiss
+    @FocusState var isFocused: Bool
+    
+    var body: some View {
+        NavigationView {
+            List {
+                // ゲーム数入力
+                unitTextFieldGamesInput(title: "実ゲーム数", inputValue: $danvine.inputGame)
+                    .focused($isFocused)
+                    .toolbar {
+                        ToolbarItem(placement: .keyboard) {
+                            HStack {
+                                Spacer()
+                                Button(action: {
+                                    isFocused = false
+                                }, label: {
+                                    Text("完了")
+                                        .fontWeight(.bold)
+                                })
+                            }
+                        }
+                    }
+                // サークルピッカー横並び １段目
+                HStack {
+                    // ボーナス種類
+                    unitPickerCircleString(
+                        title: "ボーナス種類",
+                        selected: $danvine.selectedBonus,
+                        selectList: danvine.selectListBonus
+                    )
+                    // 当選契機
+                    unitPickerCircleString(
+                        title: "当選契機",
+                        selected: $danvine.selectedTrigger,
+                        selectList: danvine.selectListTrigger
+                    )
+                }
+                // サークルピッカー横並び ２段目
+                HStack {
+                    // ST当否
+                    if danvine.selectedBonus == danvine.selectListBonus[0] {
+                        // フェラリオ時の選択肢
+                        unitPickerCircleString(
+                            title: "ST当否",
+                            selected: $danvine.selectedStHit,
+                            selectList: danvine.selectListStHit
+                        )
+                    } else {
+                        // フェラリオ以外の選択肢
+                        unitPickerCircleString(
+                            title: "ST当否",
+                            selected: $danvine.selectedStHit,
+                            selectList: danvine.selectListStHitOraHyper
+                        )
+                    }
+                    Rectangle()
+                        .frame(maxWidth: .infinity)
+                        .foregroundStyle(Color.clear)
+                }
+                // 登録ボタン
+                HStack {
+                    Spacer()
+                    Button {
+                        danvine.addDataHistory()
+                        UINotificationFeedbackGenerator().notificationOccurred(.success)
+                        dismiss()
+                    } label: {
+                        Text("登録")
+                            .fontWeight(.bold)
+                    }
+                    Spacer()
+                }
+            }
+            .navigationTitle("履歴登録")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .automatic) {
+                    Button(action: {
+                        dismiss()
+                    }, label: {
+                        Text("閉じる")
+                            .fontWeight(.bold)
+                    })
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    danvineViewHistory()
+}
--- SetteiSaCountApp/danvine/danvineViewTop.swift
+++ SetteiSaCountApp/danvine/danvineViewTop.swift
@@ -6,10 +6,153 @@
 //
 
 import SwiftUI
+import TipKit
+
+
+// /////////////////////////
+// 変数
+// /////////////////////////
+class Danvine: ObservableObject {
+    // ////////////////////////
+    // 履歴
+    // ////////////////////////
+    // 選択肢の設定
+    let selectListBonus = [
+        "フェラリオ",
+        "オーラ",
+        "ハイパー"
+    ]
+    let selectListTrigger = [
+        "ミッション",
+        "アタック",
+        "規定Pt",
+        "直撃",
+        "天井",
+        "フリーズ",
+        "その他"
+    ]
+    let selectListStHit = [
+        "当選",
+        "ハズレ"
+    ]
+    let selectListStHitOraHyper = [
+        "当選"
+    ]
+    // 選択結果の設定
+    @Published var inputGame = 0
+    @Published var selectedBonus = "オーラ"
+    @Published var selectedTrigger = "ミッション"
+    @Published var selectedStHit = "当選"
+    // //// 配列の設定
+    // ゲーム数配列
+    let gameArrayKey = "danvineGameArrayKey"
+    @AppStorage("danvineGameArrayKey") var gameArrayData: Data?
+    // ボーナス種類配列
+    let bonusArrayKey = "danvineBonusArrayKey"
+    @AppStorage("danvineBonusArrayKey") var bonusArrayData: Data?
+    // 当選契機配列
+    let triggerArrayKey = "danvineTriggerArrayKey"
+    @AppStorage("danvineTriggerArrayKey") var triggerArrayData: Data?
+    // ST当否配列
+    let stHitArrayKey = "danvineStHitArrayKey"
+    @AppStorage("danvineStHitArrayKey") var stHitArrayData: Data?
+    // //// 結果集計
+    @AppStorage("danvinePlayGameSum") var playGameSum = 0
+    @AppStorage("danvineBonusCount") var bonusCount = 0
+    @AppStorage("danvineStCount") var stCount = 0
+    
+    // 1行削除
+    func removeLastHistory() {
+        arrayIntRemoveLast(arrayData: gameArrayData, key: gameArrayKey)
+        arrayStringRemoveLast(arrayData: bonusArrayData, key: bonusArrayKey)
+        arrayStringRemoveLast(arrayData: triggerArrayData, key: triggerArrayKey)
+        arrayStringRemoveLast(arrayData: stHitArrayData, key: stHitArrayKey)
+        bonusCount = arrayIntAllDataCount(arrayData: gameArrayData)
+        stCount = arrayStringDataCount(arrayData: stHitArrayData, countString: selectListStHit[0])
+        playGameSum = arrayIntAllDataSum(arrayData: gameArrayData)
+        inputGame = 0
+        selectedBonus = "オーラ"
+        selectedTrigger = "ミッション"
+        selectedStHit = "当選"
+    }
+    // データ追加
+    func addDataHistory() {
+        arrayIntAddData(arrayData: gameArrayData, addData: inputGame, key: gameArrayKey)
+        arrayStringAddData(arrayData: bonusArrayData, addData: selectedBonus, key: bonusArrayKey)
+        arrayStringAddData(arrayData: triggerArrayData, addData: selectedTrigger, key: triggerArrayKey)
+        arrayStringAddData(arrayData: stHitArrayData, addData: selectedStHit, key: stHitArrayKey)
+        bonusCount = arrayIntAllDataCount(arrayData: gameArrayData)
+        stCount = arrayStringDataCount(arrayData: stHitArrayData, countString: selectListStHit[0])
+        playGameSum = arrayIntAllDataSum(arrayData: gameArrayData)
+        inputGame = 0
+        selectedBonus = "オーラ"
+        selectedTrigger = "ミッション"
+        selectedStHit = "当選"
+    }
+    
+    func resetHistory() {
+        arrayIntRemoveAll(arrayData: gameArrayData, key: gameArrayKey)
+        arrayStringRemoveAll(arrayData: bonusArrayData, key: bonusArrayKey)
+        arrayStringRemoveAll(arrayData: triggerArrayData, key: triggerArrayKey)
+        arrayStringRemoveAll(arrayData: stHitArrayData, key: stHitArrayKey)
+        bonusCount = arrayIntAllDataCount(arrayData: gameArrayData)
+        stCount = arrayStringDataCount(arrayData: stHitArrayData, countString: selectListStHit[0])
+        playGameSum = arrayIntAllDataSum(arrayData: gameArrayData)
+        inputGame = 0
+        selectedBonus = "オーラ"
+        selectedTrigger = "ミッション"
+        selectedStHit = "当選"
+        minusCheck = false
+    }
+    
+    // ////////////////////////
+    // 共通
+    // ////////////////////////
+    @AppStorage("danvineMinusCheck") var minusCheck: Bool = false
+    @AppStorage("danvineSelectedMemory") var selectedMemory = "メモリー1"
+    
+    func resetAll() {
+        resetHistory()
+    }
+}
+
+
 
 struct danvineViewTop: View {
+    @ObservedObject var danvine = Danvine()
+    @State var isShowAlert: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List {
+                Section {
+                    // 初当り履歴
+                    NavigationLink(destination: danvineViewHistory()) {
+                        unitLabelMenu(
+                            imageSystemName: "pencil.and.list.clipboard",
+                            textBody: "ボーナス,ST初当り履歴")
+                    }
+                } header: {
+                    unitLabelMachineTopTitle(machineName: "ダンバイン", titleFont: .title2)
+                }
+            }
+        }
+        .navigationTitle("メニュー")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            HStack {
+                HStack {
+                    // データ読み出し
+//                    unitButtonLoadMemory(loadView: AnyView(lupinSubViewLoadMemory()))
+                    // データ保存
+//                    unitButtonSaveMemory(saveView: AnyView(lupinSubViewSaveMemory()))
+                }
+                .popoverTip(tipUnitButtonMemory())
+                // データリセット
+                unitButtonReset(isShowAlert: $isShowAlert, action: danvine.resetAll, message: "この機種のデータを全てリセットします")
+                    .popoverTip(tipUnitButtonReset())
+            }
+        }
     }
 }
 
