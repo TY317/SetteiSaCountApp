e4658dc3170722a76ceb75d6681dda8bb0bdd9f3
TY317 <musicman_stingray_f91@yahoo.co.jp>
2026-01-10 08:24:12 +0900

    シェイク　メモリー実装

--- SetteiSaCountApp/shake/shakeClass.swift
+++ SetteiSaCountApp/shake/shakeClass.swift
@@ -181,3 +181,117 @@ class Shake: ObservableObject {
         resetScreen()
     }
 }
+
+
+class ShakeMemory1: ObservableObject {
+    @AppStorage("shakeGameNumberStartMemory1") var gameNumberStart: Int = 0
+    @AppStorage("shakeGameNumberCurrentMemory1") var gameNumberCurrent: Int = 0
+    @AppStorage("shakeGameNumberPlayMemory1") var gameNumberPlay: Int = 0
+    @AppStorage("shakeKoyakuCountBellMemory1") var koyakuCountBell: Int = 0
+    @AppStorage("shakeKoyakuCountCherryMemory1") var koyakuCountCherry: Int = 0
+    @AppStorage("shakeKoyakuCountSuikaMemory1") var koyakuCountSuika: Int = 0
+    @AppStorage("shakeNormalGameMemory1") var normalGame: Int = 0
+    @AppStorage("shakeBonusCountBigMemory1") var bonusCountBig: Int = 0
+    @AppStorage("shakeBonusCountRegMemory1") var bonusCountReg: Int = 0
+    @AppStorage("shakeBonusCountSumMemory1") var bonusCountSum: Int = 0
+    @AppStorage("shakeIdenBonusCountSuikaMemory1") var idenBonusCountSuika: Int = 0
+    @AppStorage("shakeIdenBonusCountBellMemory1") var idenBonusCountBell: Int = 0
+    @AppStorage("shakeIdenBonusCountSpecialIMemory1") var idenBonusCountSpecialI: Int = 0
+    @AppStorage("shakeIdenBonusCountSumMemory1") var idenBonusCountSum: Int = 0
+    @AppStorage("shakeVoiceCountDefaultMemory1") var voiceCountDefault: Int = 0
+    @AppStorage("shakeVoiceCountHighJakuMemory1") var voiceCountHighJaku: Int = 0
+    @AppStorage("shakeVoiceCountHighChuMemory1") var voiceCountHighChu: Int = 0
+    @AppStorage("shakeVoiceCountHighKyoMemory1") var voiceCountHighKyo: Int = 0
+    @AppStorage("shakeVoiceCountOver5Memory1") var voiceCountOver5: Int = 0
+    @AppStorage("shakeVoiceCountSumMemory1") var voiceCountSum: Int = 0
+    @AppStorage("shakeJacCountEndMemory1") var jacCountEnd: Int = 0
+    @AppStorage("shakeJacCountContinueMemory1") var jacCountContinue: Int = 0
+    @AppStorage("shakeJacCountSpecialMemory1") var jacCountSpecial: Int = 0
+    @AppStorage("shakeJacCountSumMemory1") var jacCountSum: Int = 0
+    @AppStorage("shakeScreenCountDefaultMemory1") var screenCountDefault: Int = 0
+    @AppStorage("shakeScreenCountHighJakuMemory1") var screenCountHighJaku: Int = 0
+    @AppStorage("shakeScreenCountHighChuMemory1") var screenCountHighChu: Int = 0
+    @AppStorage("shakeScreenCountHighKyoMemory1") var screenCountHighKyo: Int = 0
+    @AppStorage("shakeScreenCountOver6Memory1") var screenCountOver6: Int = 0
+    @AppStorage("shakeScreenCountOver1000Memory1") var screenCountOver1000: Int = 0
+    @AppStorage("shakeScreenCountOver1500Memory1") var screenCountOver1500: Int = 0
+    @AppStorage("shakeScreenCountSumMemory1") var screenCountSum: Int = 0
+    @AppStorage("shakeMemoMemory1") var memo = ""
+    @AppStorage("shakeDateMemory1") var dateDouble = 0.0
+}
+
+
+class ShakeMemory2: ObservableObject {
+    @AppStorage("shakeGameNumberStartMemory2") var gameNumberStart: Int = 0
+    @AppStorage("shakeGameNumberCurrentMemory2") var gameNumberCurrent: Int = 0
+    @AppStorage("shakeGameNumberPlayMemory2") var gameNumberPlay: Int = 0
+    @AppStorage("shakeKoyakuCountBellMemory2") var koyakuCountBell: Int = 0
+    @AppStorage("shakeKoyakuCountCherryMemory2") var koyakuCountCherry: Int = 0
+    @AppStorage("shakeKoyakuCountSuikaMemory2") var koyakuCountSuika: Int = 0
+    @AppStorage("shakeNormalGameMemory2") var normalGame: Int = 0
+    @AppStorage("shakeBonusCountBigMemory2") var bonusCountBig: Int = 0
+    @AppStorage("shakeBonusCountRegMemory2") var bonusCountReg: Int = 0
+    @AppStorage("shakeBonusCountSumMemory2") var bonusCountSum: Int = 0
+    @AppStorage("shakeIdenBonusCountSuikaMemory2") var idenBonusCountSuika: Int = 0
+    @AppStorage("shakeIdenBonusCountBellMemory2") var idenBonusCountBell: Int = 0
+    @AppStorage("shakeIdenBonusCountSpecialIMemory2") var idenBonusCountSpecialI: Int = 0
+    @AppStorage("shakeIdenBonusCountSumMemory2") var idenBonusCountSum: Int = 0
+    @AppStorage("shakeVoiceCountDefaultMemory2") var voiceCountDefault: Int = 0
+    @AppStorage("shakeVoiceCountHighJakuMemory2") var voiceCountHighJaku: Int = 0
+    @AppStorage("shakeVoiceCountHighChuMemory2") var voiceCountHighChu: Int = 0
+    @AppStorage("shakeVoiceCountHighKyoMemory2") var voiceCountHighKyo: Int = 0
+    @AppStorage("shakeVoiceCountOver5Memory2") var voiceCountOver5: Int = 0
+    @AppStorage("shakeVoiceCountSumMemory2") var voiceCountSum: Int = 0
+    @AppStorage("shakeJacCountEndMemory2") var jacCountEnd: Int = 0
+    @AppStorage("shakeJacCountContinueMemory2") var jacCountContinue: Int = 0
+    @AppStorage("shakeJacCountSpecialMemory2") var jacCountSpecial: Int = 0
+    @AppStorage("shakeJacCountSumMemory2") var jacCountSum: Int = 0
+    @AppStorage("shakeScreenCountDefaultMemory2") var screenCountDefault: Int = 0
+    @AppStorage("shakeScreenCountHighJakuMemory2") var screenCountHighJaku: Int = 0
+    @AppStorage("shakeScreenCountHighChuMemory2") var screenCountHighChu: Int = 0
+    @AppStorage("shakeScreenCountHighKyoMemory2") var screenCountHighKyo: Int = 0
+    @AppStorage("shakeScreenCountOver6Memory2") var screenCountOver6: Int = 0
+    @AppStorage("shakeScreenCountOver1000Memory2") var screenCountOver1000: Int = 0
+    @AppStorage("shakeScreenCountOver1500Memory2") var screenCountOver1500: Int = 0
+    @AppStorage("shakeScreenCountSumMemory2") var screenCountSum: Int = 0
+    @AppStorage("shakeMemoMemory2") var memo = ""
+    @AppStorage("shakeDateMemory2") var dateDouble = 0.0
+}
+
+
+class ShakeMemory3: ObservableObject {
+    @AppStorage("shakeGameNumberStartMemory3") var gameNumberStart: Int = 0
+    @AppStorage("shakeGameNumberCurrentMemory3") var gameNumberCurrent: Int = 0
+    @AppStorage("shakeGameNumberPlayMemory3") var gameNumberPlay: Int = 0
+    @AppStorage("shakeKoyakuCountBellMemory3") var koyakuCountBell: Int = 0
+    @AppStorage("shakeKoyakuCountCherryMemory3") var koyakuCountCherry: Int = 0
+    @AppStorage("shakeKoyakuCountSuikaMemory3") var koyakuCountSuika: Int = 0
+    @AppStorage("shakeNormalGameMemory3") var normalGame: Int = 0
+    @AppStorage("shakeBonusCountBigMemory3") var bonusCountBig: Int = 0
+    @AppStorage("shakeBonusCountRegMemory3") var bonusCountReg: Int = 0
+    @AppStorage("shakeBonusCountSumMemory3") var bonusCountSum: Int = 0
+    @AppStorage("shakeIdenBonusCountSuikaMemory3") var idenBonusCountSuika: Int = 0
+    @AppStorage("shakeIdenBonusCountBellMemory3") var idenBonusCountBell: Int = 0
+    @AppStorage("shakeIdenBonusCountSpecialIMemory3") var idenBonusCountSpecialI: Int = 0
+    @AppStorage("shakeIdenBonusCountSumMemory3") var idenBonusCountSum: Int = 0
+    @AppStorage("shakeVoiceCountDefaultMemory3") var voiceCountDefault: Int = 0
+    @AppStorage("shakeVoiceCountHighJakuMemory3") var voiceCountHighJaku: Int = 0
+    @AppStorage("shakeVoiceCountHighChuMemory3") var voiceCountHighChu: Int = 0
+    @AppStorage("shakeVoiceCountHighKyoMemory3") var voiceCountHighKyo: Int = 0
+    @AppStorage("shakeVoiceCountOver5Memory3") var voiceCountOver5: Int = 0
+    @AppStorage("shakeVoiceCountSumMemory3") var voiceCountSum: Int = 0
+    @AppStorage("shakeJacCountEndMemory3") var jacCountEnd: Int = 0
+    @AppStorage("shakeJacCountContinueMemory3") var jacCountContinue: Int = 0
+    @AppStorage("shakeJacCountSpecialMemory3") var jacCountSpecial: Int = 0
+    @AppStorage("shakeJacCountSumMemory3") var jacCountSum: Int = 0
+    @AppStorage("shakeScreenCountDefaultMemory3") var screenCountDefault: Int = 0
+    @AppStorage("shakeScreenCountHighJakuMemory3") var screenCountHighJaku: Int = 0
+    @AppStorage("shakeScreenCountHighChuMemory3") var screenCountHighChu: Int = 0
+    @AppStorage("shakeScreenCountHighKyoMemory3") var screenCountHighKyo: Int = 0
+    @AppStorage("shakeScreenCountOver6Memory3") var screenCountOver6: Int = 0
+    @AppStorage("shakeScreenCountOver1000Memory3") var screenCountOver1000: Int = 0
+    @AppStorage("shakeScreenCountOver1500Memory3") var screenCountOver1500: Int = 0
+    @AppStorage("shakeScreenCountSumMemory3") var screenCountSum: Int = 0
+    @AppStorage("shakeMemoMemory3") var memo = ""
+    @AppStorage("shakeDateMemory3") var dateDouble = 0.0
+}
