df4a1248e726f3e8f5646ca3d83ff09d51717b07
TY317 <musicman_stingray_f91@yahoo.co.jp>
2026-01-12 22:05:02 +0900

    攻殻機動隊　トップページ作成

--- SetteiSaCountApp/kokakukidotai/kokakukidotaiClass.swift
+++ SetteiSaCountApp/kokakukidotai/kokakukidotaiClass.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
+import Combine
+
+class Kokakukidotai: ObservableObject {
+    
+    // -----------
+    // 共通
+    // -----------
+    let machineName: String = "攻殻機動隊"
+    @AppStorage("kokakukidotaiMinusCheck") var minusCheck: Bool = false
+    @AppStorage("kokakukidotaiSelectedMemory") var selectedMemory = "メモリー1"
+    
+    func resetAll() {
+        
+    }
+}
---
+++ SetteiSaCountApp/kokakukidotai/kokakukidotaiView95Ci.swift
@@ -0,0 +1,18 @@
+//
+//  kokakukidotaiView95Ci.swift
+//  SetteiSaCountApp
+//
+//  Created by 横田徹 on 2026/01/12.
+//
+
+import SwiftUI
+
+struct kokakukidotaiView95Ci: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    kokakukidotaiView95Ci()
+}
