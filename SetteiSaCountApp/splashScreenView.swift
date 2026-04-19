b3edb7d83dfe2b5b7c28cce7179a901ae85c80fe
TY317 <musicman_stingray_f91@yahoo.co.jp>
2026-04-19 11:33:35 +0900

    [共通]ver3.24.0開発完了

--- SetteiSaCountApp/splashScreenView.swift
+++ SetteiSaCountApp/splashScreenView.swift
@@ -66,7 +66,7 @@ struct splashScreenView: View {
                         // ----- リリース前にコメントアウト！！！
 //                        common.firstLaunchAppVersion = nil
 //                        common.lastLaunchAppVersion = nil
-                        common.lastLaunchAppVersion = "3.23.0"
+//                        common.lastLaunchAppVersion = "3.23.0"
                         // --------------------------------
 //                        common.ver3200FirstLaunch()
                         common.ver3210FirstLaunch()
