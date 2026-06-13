0d9c4e2b28904b74f6c02963f0460866f67cbee4
TY317 <musicman_stingray_f91@yahoo.co.jp>
2026-06-13 20:57:44 +0900

    [機能]バイオRE3 通常時ぺーじリセットボタン追加

--- SetteiSaCountApp/bioRe3/bioRe3ViewNormal.swift
+++ SetteiSaCountApp/bioRe3/bioRe3ViewNormal.swift
@@ -14,6 +14,19 @@ struct bioRe3ViewNormal: View {
     @EnvironmentObject var common: commonVar
     let items: [String] = ["弱🍒・🍉","チャンス目・強🍒",]
     @State var selectedItem: String = "弱🍒・🍉"
+    @State var isShowAlert: Bool = false
+    @FocusState var isFocused: Bool
+    @State private var orientation: UIDeviceOrientation = UIDevice.current.orientation
+    @State private var lastOrientation: UIDeviceOrientation = .portrait // 直前の向き
+    let scrollViewHeightPortrait = 250.0
+    let scrollViewHeightLandscape = 150.0
+    @State var scrollViewHeight = 250.0
+    let spaceHeightPortrait = 250.0
+    let spaceHeightLandscape = 0.0
+    @State var spaceHeight = 250.0
+    let lazyVGridCountPortrait: Int = 3
+    let lazyVGridCountLandscape: Int = 5
+    @State var lazyVGridCount: Int = 3
     
     var body: some View {
         List {
@@ -162,6 +175,30 @@ struct bioRe3ViewNormal: View {
         }
         .navigationTitle("通常時")
         .navigationBarTitleDisplayMode(.inline)
+        // //// 画面の向き情報の取得部分
+        .applyOrientationHandling(
+            orientation: self.$orientation,
+            lastOrientation: self.$lastOrientation,
+            scrollViewHeight: self.$scrollViewHeight,
+            spaceHeight: self.$spaceHeight,
+            lazyVGridCount: self.$lazyVGridCount,
+            scrollViewHeightPortrait: self.scrollViewHeightPortrait,
+            scrollViewHeightLandscape: self.scrollViewHeightLandscape,
+            spaceHeightPortrait: self.spaceHeightPortrait,
+            spaceHeightLandscape: self.spaceHeightLandscape,
+            lazyVGridCountPortrait: self.lazyVGridCountPortrait,
+            lazyVGridCountLandscape: self.lazyVGridCountLandscape
+        )
+        .toolbar {
+            ToolbarItem(placement: .automatic) {
+                // //// マイナスチェック
+                unitButtonMinusCheck(minusCheck: $bioRe3.minusCheck)
+            }
+            ToolbarItem(placement: .automatic) {
+                // /// リセット
+                unitButtonReset(isShowAlert: $isShowAlert, action: bioRe3.resetNormal)
+            }
+        }
     }
 }
 
